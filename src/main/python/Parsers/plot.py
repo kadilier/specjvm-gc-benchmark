89c06f39151a8cd4faad17d417ea0ae659cf561e
Giorgos Kadilierakis <giogwgiog@gmail.com>
2018-05-17 23:35:24 +0300

    Finalizing parsers 2

--- src/main/python/Parsers/plot.py
+++ src/main/python/Parsers/plot.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 # data to plot
 n_groups = 9
-data_g1 = (90, 55, 40, 65, 0, 12, 33, 44, 55)
-data_gss = (85, 62, 54, 20, 14, 0, 32, 66, 33)
-data_ss = (44,33,23,11, 55, 22, 43, 54, 11)
+data_g1 = (168131,3460,77705,174639,10669,11673,134755,83153,195725)
+data_gss = (94796,2695,33608,91646,5347,7969,61245,60995,96728)
+data_ss = (66279,2393,35739,37166,5440,7876,42965,45004,65973)
 
 # create plot
 fig, ax = plt.subplots()
@@ -29,8 +29,8 @@ rects3 = plt.bar(index + 2*bar_width, data_g1, bar_width,
                  label='G1_HotSpot')
 
 plt.xlabel('Specjvm2008')
-plt.ylabel('Total collection time (ms)')
-plt.title('Heap Size 64MB')
+plt.ylabel('Total Reclaimed (MB)')
+plt.title('Heap Size 1024MB')
 plt.xticks(index + bar_width, ('Compiler','Compress','Crypto','Derby','Mpegaudio','Scimark.large','Serial','Sunflow','Xml'))
 plt.legend()
 
